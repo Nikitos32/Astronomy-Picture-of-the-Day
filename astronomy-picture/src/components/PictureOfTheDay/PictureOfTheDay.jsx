ee2dbde6bc34194055196aefae038fd046e01bfb
Nikita <nikita456nikita@yandex.ru>
2024-01-19 23:52:29 +0300

    fix: fix problem with button and add some style

--- astronomy-picture/src/components/PictureOfTheDay/PictureOfTheDay.jsx
+++ astronomy-picture/src/components/PictureOfTheDay/PictureOfTheDay.jsx
@@ -5,6 +5,7 @@ import classes from "./PictureOfTheDay.module.css"
 export default function PictureOfTheDay({pic, setPicture, date, isLoaded, setLoaded}) {
     
     const fetchPicture = useCallback(async () => {
+        setLoaded(true)
         fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
             .then(res => res.json())
             .then((res) => {
